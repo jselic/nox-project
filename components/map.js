ce3623139de310df482e49b147c012fb9d250051
Matjaž Ciglič <matjaz.ciglic.jst@gmail.com>
2023-04-01 23:00:50 +0200

    Change marker location

--- components/map.js
+++ components/map.js
@@ -3,8 +3,8 @@ import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export function Map(){
     let location = {
-        latitude: 46.05116231973432,
-        longitude: 14.469928934371586,
+        latitude: 46.04999890020227,
+        longitude: 14.46904593724097,
         latitudeDelta: 0.009,
         longitudeDelta: 0.009
     }
