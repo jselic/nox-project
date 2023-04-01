f561943f7d7d270142dd70639808db0771d28c98
Matjaž Ciglič <matjaz.ciglic.jst@gmail.com>
2023-04-02 01:01:33 +0200

    One working marker

--- components/map.js
+++ components/map.js
@@ -1,5 +1,6 @@
-import {StyleSheet, View} from "react-native";
+import {Linking, StyleSheet, View} from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import {Feed} from "./feed";
 
 export function Map(){
     let location = {
@@ -7,6 +8,14 @@ export function Map(){
         longitude: 14.46904593724097,
         latitudeDelta: 0.009,
         longitudeDelta: 0.009
+    }
+    let marker = {
+        _id: 123,
+        name: "Hackaton",
+        location: {
+            latitude: 46.04999890020227,
+            longitude: 14.46904593724097
+        }
     }
     return (
         <View
@@ -18,7 +27,10 @@ export function Map(){
             provider={PROVIDER_GOOGLE}
             mapType='hybrid'
             region={location}>
-                <Marker coordinate={{latitude: location.latitude, longitude: location.longitude}} />
+                <Marker
+                    coordinate={{latitude: marker.location.latitude, longitude: marker.location.longitude}}
+                    onPress={() => Linking.openURL('youtube.com')}
+                />
             </MapView>
         </View>
     )
