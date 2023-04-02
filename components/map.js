9a1c5953f7d6dcfac3848c8b46eb11affdaa14de
jselic <js4941@student.uni-lj.si>
2023-04-02 04:56:11 +0200

    The map works..... sort of

--- components/map.js
+++ components/map.js
@@ -1,6 +1,11 @@
-import {Linking, StyleSheet, View} from "react-native";
-import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import {Linking, StyleSheet, Text, View} from "react-native";
+import MapView, {PROVIDER_GOOGLE, Marker, Callout} from "react-native-maps";
 import {Feed} from "./feed";
+import challenges from '../assets/challenges.json'
+import community from '../assets/community.json'
+import {ChallengeBox} from "./challenge_box";
+import React from "react";
+import * as MapStyles from "../styles/map_styles.js"
 
 export function Map(){
     let location = {
@@ -9,14 +14,6 @@ export function Map(){
         latitudeDelta: 0.009,
         longitudeDelta: 0.009
     }
-    let marker = {
-        _id: 123,
-        name: "Hackaton",
-        location: {
-            latitude: 46.04999890020227,
-            longitude: 14.46904593724097
-        }
-    }
     return (
         <View
             style={{
@@ -24,13 +21,31 @@ export function Map(){
             }}>
             <MapView
                 style={StyleSheet.absoluteFillObject}
-            provider={PROVIDER_GOOGLE}
-            mapType='hybrid'
-            region={location}>
+                provider={PROVIDER_GOOGLE}
+                mapType='hybrid'
+                region={location}>
+                {challenges.map((challenge,index) =>(
                 <Marker
-                    coordinate={{latitude: marker.location.latitude, longitude: marker.location.longitude}}
-                    onPress={() => Linking.openURL('youtube.com')}
-                />
+                    key={index}
+                    coordinate={{latitude: challenge.location_latitude, longitude: challenge.location_longitude}}
+                >
+                <View style={MapStyles.marker1}></View>
+                    <Callout style={MapStyles.callout}>
+                        <Text>{challenge.title}</Text>
+                    </Callout>
+                </Marker>
+                ))}
+                {community.map((challenge,index) =>(
+                <Marker
+                    key={index}
+                    coordinate={{latitude: challenge.location_latitude, longitude: challenge.location_longitude}}
+                >
+                    <View style={MapStyles.marker2}></View>
+                    <Callout style={MapStyles.callout}>
+                        <Text>{challenge.title}</Text>
+                    </Callout>
+                </Marker>
+                ))}
             </MapView>
         </View>
     )
---
+++ styles/map_styles.js
@@ -0,0 +1,32 @@
+export const marker1 = {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    backgroundColor: '#2f4f4f',
+    borderColor: '#87cefa',
+    borderWidth: 2,
+    shadowColor: '#87cefa',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5,
+}
+
+export const marker2 = {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    backgroundColor: '#4f4f2f',
+    borderColor: '#87face',
+    borderWidth: 2,
+    shadowColor: '#87cefa',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5,
+}
+
+export const callout = {
+    flex: 1,
+    position: 'relative',
+}
